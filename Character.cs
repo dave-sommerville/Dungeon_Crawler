cf596c7d22e7fb7888769f340f1264447b0cf272
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-04-19 20:23:05 -0500

    NPC added

--- Character.cs
+++ Character.cs
@@ -1,16 +1,20 @@
-﻿namespace Dungeon_Crawler
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace Dungeon_Crawler
 {
     public class Character
     {
         private readonly int _nextId;
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public int ArmorClass { get; set; }
         public int Health { get; set; }
-        public Character(string name)
+        public Character(string name, string description)
         {
             Id = _nextId++;
             Name = name;
+            Description = description;
             ArmorClass = 0;
             Health = 0;
         }
---
+++ NPC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler
+{
+    public class NPC : Character
+    {
+        public string[] Dialogues { get; set; }
+        public int[] Quests { get; set; }
+        public int[] Items { get; set; }
+        public int[] Locations { get; set; }
+        public NPC(string name, string description) : base(name, description)
+        {
+            Description = "A mysterious figure.";
+            Dialogues = new string[] { "Hello, traveler!", "What brings you here?", "I have a quest for you." };
+        }
+    }
+}
