82190727a68d057f5e1fdfe798069e795cf29e68
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-04-19 20:17:01 -0500

    Battlefield and Traps added

---
+++ Battlefield.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler
+{
+    public class Battlefield : Chamber
+    {
+
+        public Battlefield(string id, string description) : base(id, description)
+        {
+            ChamberId = id;
+            Description = description;
+            NorthPassage = true;
+            SouthPassage = false;
+            EastPassage = true;
+            WestPassage = true;
+        }
+        public void DisplayBattlefield()
+        {
+            Console.WriteLine(Description);
+        }
+    }
+}
